388b303ec59f5a0615221ff5c4fc853d262b5d3d
nakaha24a <harunaosaki298@gmail.com>
2025-11-06 12:55:11 +0900

    変更

--- src/components/OrderScreen.tsx
+++ src/components/OrderScreen.tsx
@@ -101,7 +101,7 @@ const OrderScreen: React.FC<OrderScreenProps> = ({
 
     if (newOrder) {
       setShowOrderComplete(true);
-      setTimeout(() => setShowOrderComplete(false), 1500);
+      setTimeout(() => setShowOrderComplete(false), 1000);
     } else {
       // ストアからエラーメッセージを取得して表示
       const storeError = useCartStore.getState().error;
